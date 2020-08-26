5af6805a756a291717bbb554ec639b4df2d6531a
zachm1995 <zachmumbauer@gmail.com>
2020-08-26 18:03:32 -0400

    added express

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express';
 
 const main = async () => {
 	// Creates the database
@@ -10,11 +11,20 @@ const main = async () => {
 	// Creates migration
 	await orm.getMigrator().up();
 
-	const post = orm.em.create(Post, {title: "First post"});
-	await orm.em.persistAndFlush(post);
 
-	const posts = await orm.em.find(Post, {});
-	console.log(posts);
+	const app = express();
+	app.get('/', (_, res) => {
+		res.send("hello");
+	})
+
+ 	app.listen(4000, () => {
+ 		console.log('server started on localhost:4000');
+ 	})
+	// const post = orm.em.create(Post, {title: "First post"});
+	// await orm.em.persistAndFlush(post);
+
+	// const posts = await orm.em.find(Post, {});
+	// console.log(posts);
 
 }
 
