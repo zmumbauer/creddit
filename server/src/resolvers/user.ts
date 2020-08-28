10dae532ab5649d535bf3e72db63ecf2a6a431f5
zachm1995 <zachmumbauer@gmail.com>
2020-08-28 00:57:36 -0400

    added validations for username and password length

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,6 +11,7 @@ import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
 
+// Creates fields for username and password for registration and login
 @InputType()
 class UsernamePasswordInput {
 	@Field()
@@ -19,6 +20,7 @@ class UsernamePasswordInput {
 	password: string;
 }
 
+// Object to return an error message specific to a user-input field
 @ObjectType()
 class FieldError {
 	@Field()
@@ -27,6 +29,9 @@ class FieldError {
 	message: string;
 }
 
+// Object that is returned on login.
+// Returns a User object upon success
+// Returns a FieldError object upon failure
 @ObjectType()
 class UserResponse {
 	@Field(() => [FieldError], { nullable: true })
@@ -41,18 +46,59 @@ export class UserResolver {
 
 	// Creates a new user with provided username and password through options object
 	// Returns the created user
-	@Mutation(() => User)
+	@Mutation(() => UserResponse)
 	async register(
 		@Arg("options") options: UsernamePasswordInput,
 		@Ctx() { em }: MyContext
-	) {
+	): Promise<UserResponse> {
+
+		// Check that username has at least 4 characters
+		if (options.username.length < 4) {
+			return {
+				errors: [
+					{
+						field: "Username",
+						message: "Username must have at least 4 characters"
+					},
+				]
+			}
+		}
+
+		// Check that password has at least 8 characters
+		if (options.password.length < 8) {
+			return {
+				errors: [
+					{
+						field: "Password",
+						message: "Password must have at least 8 characters"
+					},
+				]
+			}
+		}
+
 		const hashedPassword = await argon2.hash(options.password);
 		const user = em.create(User, {
 			username: options.username,
 			password: hashedPassword,
 		});
-		await em.persistAndFlush(user);
-		return user;
+		try {
+			await em.persistAndFlush(user);	
+		} catch(err) {
+
+			// Duplicate username
+			if (err.code === "23505" || err.detail.include("already exists")) {
+				return {
+					errors: [
+						{
+							field: "Username",
+							message: "Username has already been claimed"
+						},
+					]
+				}
+			}
+		}
+		
+		return { user };
 	}
 
 	// Handles user login based on provided username and password
@@ -61,29 +107,36 @@ export class UserResolver {
 		@Arg("options") options: UsernamePasswordInput,
 		@Ctx() { em }: MyContext
 	): Promise<UserResponse> {
+
+		// Checks for username in database
 		const user = await em.findOne(User, { username: options.username });
-		
+
+		// If a user with the provided username doesn't exist, returns a FieldError
 		if (!user) {
 			return {
 				errors: [
 					{
-						field: "username",
-						message: "username not found",
+						field: "Username",
+						message: "Username not found",
 					},
 				],
 			};
 		}
+
+		// Validates stored password with supplied password
 		const validatePassword = await argon2.verify(user.password, options.password);
 		if (!validatePassword) {
 			return {
 				errors: [
 					{
-						field: 'password',
-						message: 'incorrect password',
+						field: 'Password',
+						message: 'Incorrect password',
 					},
 				]
 			}
 		}
+
+		// Upon successful login, returns the User object
 		return {
 			user,
 		};
