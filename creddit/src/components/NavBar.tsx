ab9f023220ff85899559e6fddc6f49686afc95c3
zachm1995 <zachmumbauer@gmail.com>
2020-08-30 10:51:04 -0400

    added user logout in graphql and front end

--- creddit/src/components/NavBar.tsx
+++ creddit/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box, Link, Flex, Button } from "@chakra-ui/core";
 import NextLink from "next/link";
-import { useMeQuery } from "../generated/graphql";
+import { useMeQuery, useLogoutMutation } from "../generated/graphql";
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery();
   let body = null;
 
@@ -33,7 +34,13 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
     body = (
       <Flex>
         <Box mr={5}>{data.me.username}</Box>
-        <Button variant="link">logout</Button>
+        <Button
+          variant="link"
+          onClick={() => logout()}
+          isLoading={logoutFetching}
+        >
+          logout
+        </Button>
       </Flex>
     );
   }
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,35 +1,35 @@
 import {
-	Resolver,
-	Mutation,
-	Arg,
-	InputType,
-	Field,
-	Ctx,
-	ObjectType,
-	Query
+  Resolver,
+  Mutation,
+  Arg,
+  InputType,
+  Field,
+  Ctx,
+  ObjectType,
+  Query,
 } from "type-graphql";
 import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
-import { EntityManager } from '@mikro-orm/postgresql';
+import { EntityManager } from "@mikro-orm/postgresql";
 import { COOKIE_NAME } from "../constants";
 
 // Creates fields for username and password for registration and login
 @InputType()
 class UsernamePasswordInput {
-	@Field()
-	username: string;
-	@Field()
-	password: string;
+  @Field()
+  username: string;
+  @Field()
+  password: string;
 }
 
 // Object to return an error message specific to a user-input field
 @ObjectType()
 class FieldError {
-	@Field()
-	field: string;
-	@Field()
-	message: string;
+  @Field()
+  field: string;
+  @Field()
+  message: string;
 }
 
 // Object that is returned on login.
@@ -37,166 +37,163 @@ class FieldError {
 // Returns a FieldError object upon failure
 @ObjectType()
 class UserResponse {
-	@Field(() => [FieldError], { nullable: true })
-	errors?: FieldError[];
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
 
-	@Field(() => User, { nullable: true })
-	user?: User;
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
 export class UserResolver {
+  // Returns a user if stored in session (logged in)
+  // Returns null if no user is authenticated
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { em, req }: MyContext) {
+    if (!req.session.userId) {
+      return null;
+    }
 
-	// Returns a user if stored in session (logged in)
-	// Returns null if no user is authenticated
-	@Query(() => User, { nullable: true })
-	async me (
-		@Ctx() { em, req }: MyContext
-	) {
-		if (!req.session.userId) {
-			return null;
-		}
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
 
-		const user = await em.findOne(User, { id: req.session.userId});
-		return user;
-	}
+  // Creates a new user with provided username and password through options object
+  // Returns the created user
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    // Check that username has at least 4 characters
+    if (options.username.length < 4) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username must have at least 4 characters",
+          },
+        ],
+      };
+    }
 
+    // Check that password has at least 8 characters
+    if (options.password.length < 8) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Password must have at least 8 characters",
+          },
+        ],
+      };
+    }
 
-	// Creates a new user with provided username and password through options object
-	// Returns the created user
-	@Mutation(() => UserResponse)
-	async register(
-		@Arg("options") options: UsernamePasswordInput,
-		@Ctx() { em, req }: MyContext
-	): Promise<UserResponse> {
+    // Create a hashed password using the user supplied password
+    const hashedPassword = await argon2.hash(options.password);
 
-		// Check that username has at least 4 characters
-		if (options.username.length < 4) {
-			return {
-				errors: [
-					{
-						field: "username",
-						message: "Username must have at least 4 characters"
-					},
-				]
-			}
-		}
+    // Insert the user into the database
+    let user;
+    try {
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+      user = result[0];
 
-		// Check that password has at least 8 characters
-		if (options.password.length < 8) {
-			return {
-				errors: [
-					{
-						field: "password",
-						message: "Password must have at least 8 characters"
-					},
-				]
-			}
-		}
+      // Rename columns from knex
+      user.createdAt = user.created_at;
+      user.updatedAt = user.updated_at;
+    } catch (err) {
+      // Duplicate username
+      if (err.detail.includes("already exists")) {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "Username has already been claimed",
+            },
+          ],
+        };
+      }
+    }
 
-		// Create a hashed password using the user supplied password
-		const hashedPassword = await argon2.hash(options.password);
+    // stores user in session
+    req.session.userID = user.id;
 
-		// Insert the user into the database
-		let user;
-		try {
-			const result = await (em as EntityManager).createQueryBuilder(User).getKnexQuery().insert(
-				{
-					username: options.username,
-					password: hashedPassword,
-					created_at: new Date(),
-					updated_at: new Date(),
-				}
-			).returning('*');
-			user = result[0];
+    return { user };
+  }
 
-			// Rename columns from knex
-			user.createdAt = user.created_at;
-			user.updatedAt = user.updated_at;
-		} catch(err) {
-			// Duplicate username
-			if (err.detail.includes("already exists")) {
-				return {
-					errors: [
-						{
-							field: "username",
-							message: "Username has already been claimed"
-						},
-					]
-				}
-			}
-		}
-		
-		// stores user in session
-		req.session.userID = user.id;
+  // Handles user login based on provided username and password
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    // Checks for username in database
+    const user = await em.findOne(User, { username: options.username });
 
-		return { user };
-	}
+    // If a user with the provided username doesn't exist, returns a FieldError
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username not found",
+          },
+        ],
+      };
+    }
 
-	// Handles user login based on provided username and password
-	@Mutation(() => UserResponse)
-	async login(
-		@Arg("options") options: UsernamePasswordInput,
-		@Ctx() { em, req }: MyContext
-	): Promise<UserResponse> {
+    // Validates stored password with supplied password
+    const validatePassword = await argon2.verify(
+      user.password,
+      options.password
+    );
+    if (!validatePassword) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Incorrect password",
+          },
+        ],
+      };
+    }
 
-		// Checks for username in database
-		const user = await em.findOne(User, { username: options.username });
+    req.session.userId = user.id;
 
-		// If a user with the provided username doesn't exist, returns a FieldError
-		if (!user) {
-			return {
-				errors: [
-					{
-						field: "username",
-						message: "Username not found",
-					},
-				],
-			};
-		}
+    // Upon successful login, returns the User object
+    return {
+      user,
+    };
+  }
 
-		// Validates stored password with supplied password
-		const validatePassword = await argon2.verify(user.password, options.password);
-		if (!validatePassword) {
-			return {
-				errors: [
-					{
-						field: 'password',
-						message: 'Incorrect password',
-					},
-				]
-			}
-		}
+  // Handles unauthentication of user
+  // Returns true upon success; false on failure
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        // If there is an error, log to console
+        // Return false
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
 
-
-		req.session.userId = user.id;
-
-		// Upon successful login, returns the User object
-		return {
-			user,
-		};
-	}
-
-	// Handles unauthentication of user
-	// Returns true upon success; false on failure
-	@Mutation(() => Boolean)
-	logout(
-		@Ctx() { req, res }: MyContext
-	) {
-		return new Promise(resolve => req.session.destroy(err => {
-
-			// If there is an error, log to console
-			// Return false
-			if (err) {
-				console.log(err);
-				resolve(false);
-				return;
-			}
-
-			// If no errors, remove the user authentication cookie from the session
-			// Return true
-			res.clearCookie(COOKIE_NAME);
-			resolve(true);
-		})) 
-	}
+        // If no errors, remove the user authentication cookie from the session
+        // Return true
+        res.clearCookie(COOKIE_NAME);
+        resolve(true);
+      })
+    );
+  }
 }
